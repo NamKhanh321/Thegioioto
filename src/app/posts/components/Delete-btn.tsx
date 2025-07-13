88e1859545b75e918dc80eed82ba5d9e55de1a2b
NamKhanh321 <129375652+NamKhanh321@users.noreply.github.com>
2025-07-13 17:43:30 +0700

    remove prisma

--- src/app/posts/components/Delete-btn.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import { deletePost } from "@/actions/actions";
-
-export function DeleteButton({postId} : {postId : number}){
-    return (                    
-    <button onClick={() => deletePost({PostId: postId})} className="text-red-500 hover:text-amber-800">X</button>
-    )
-}
